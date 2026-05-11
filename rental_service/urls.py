b0e56f3920b1b4b35bc66a4d06177bf7f145003e
alaa <lolomamdo2006@gmail.com>
2026-05-11 23:53:00 +0300

    i add yars insert and select

--- rental_service/urls.py
+++ rental_service/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('equipment/', views.equipment_list, name='equipment_list'),
+    path('', views.equipment_list, name='equipment_list'),
     path('insertEquipment/', views.insert_equipment, name='insert_equipment'),
+    path('insertYard/', views.insert_yard, name='insert_yard'),
+    path('yard/', views.yard_list, name='yard_list'),
 ]
--- rental_service/views.py
+++ rental_service/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.db import connection
 
-
+from django.db import IntegrityError
 def equipment_list(request):
     with connection.cursor() as cursor:
      
@@ -11,27 +11,98 @@ def equipment_list(request):
         rows = cursor.fetchall()
     
     return render(request, 'rental_service\equipment.html', {'equipment': rows})
+
 def insert_equipment(request):
+
     if request.method == "POST":
+
         e_id = request.POST.get('Equipment_ID')
         model = request.POST.get('Model')
         power = request.POST.get('Engine_power')
         rate = request.POST.get('Hourly_rental_rate')
         status = request.POST.get('Status')
         yard_id = request.POST.get('Yard_ID')
+
         if yard_id == "" or yard_id is None:
             yard_id = None
+
+        try:
+            with connection.cursor() as cursor:
+
+                cursor.execute(
+                    """
+                    INSERT INTO Equipment
+                    (Equipment_ID, Model, Engine_power,
+                    Hourly_rental_rate, Status, Yard_ID)
+
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                    """,
+                    [e_id, model, power, rate, status, yard_id]
+                )
+                connection.commit()
+
+            return render(
+                request,
+                'rental_service/insertEquipment.html',
+                {'message': 'Inserted Successfully','color':'rgb(101, 192, 27)'}
+            )
+
+        except IntegrityError:
+
+            return render(
+                request,
+                'rental_service/insertEquipment.html',
+                {'message': 'Data Invalid','color':'rgb(192, 27, 27)'}
+            )
+
+    return render(request, 'rental_service/insertEquipment.html')
+
+# 
+
+# try:
+#     with connection.cursor() as cursor:
+#         cursor.execute("INSERT INTO Equipment ...", [e_id, ...])
+#         connection.commit()
+# except IntegrityError:
+#     # هنا بنقفش الـ Error لو الـ ID اتكرر
+#     return render(request, 'insert.html', {'message': 'الـ ID ده مستخدم قبل كدة، جرب غيره!'})
+
+def insert_yard(request):
+    yard_id=request.POST.get('Yard_ID')
+    local=request.POST.get('Location')
+    cap=request.POST.get('Capacity')
+    try:
         with connection.cursor() as cursor:
-            cursor.execute("SELECT Equipment_ID FROM Equipment WHERE Equipment_ID = %s", [e_id])
-            existing_id = cursor.fetchone()
-            if existing_id:
-                return render(request, 'rental_service/insertEquipment.html', {'message':"Id is exist"
-                })
-        with connection.cursor() as cursor:
-          cursor.execute(
-            "INSERT INTO Equipment (Equipment_ID, Model, Engine_power, Hourly_rental_rate, Status, Yard_ID)"
-            "VALUES(%s, %s,%s,%s,%s,%s)",
-            [e_id, model, power, rate, status, yard_id ]
-           )
-        connection.commit()
-    return render(request, 'rental_service\insertEquipment.html')
+
+                cursor.execute(
+                    """
+                    INSERT INTO Yard
+                    (Yard_ID,Location, Capacity)
+
+                    VALUES (%s, %s, %s)
+                    """,
+                    [ yard_id,local, cap]
+                )
+                connection.commit()
+
+        return render(
+                request,
+                'rental_service/insertYard.html',
+                {'messageyard': 'Inserted Successfully','color':'rgb(101, 192, 27)'}
+            )
+
+    except IntegrityError:
+            return render(
+                request,
+                'rental_service/insertYard.html',
+                {'messageyard': 'Data Invalid','color':'rgb(192, 27, 27)'}
+            )
+
+    return render(request, 'rental_service/insertYard.html')
+def yard_list(request):
+    with connection.cursor() as cursor:
+     
+        cursor.execute("SELECT * FROM Yard")
+        rows = cursor.fetchall()
+    
+    return render(request, 'rental_service\yard.html', {'yard': rows})
